c15739c77412a50d5ecf99cb6c117862d2664295
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-08-20 01:48:34 -0400

    update obsidian-clipper install script

--- scripts/install-obsidian-clipper.ps1
+++ scripts/install-obsidian-clipper.ps1
@@ -1,12 +1,15 @@
 # set github_api_token envvar for lastversion
-# $github_api_token = <PAT>
+# $github_api_token =
 
 pip install lastversion
 
 lastversion jplattel/obsidian-clipper --pre --download obsidian-clipper.zip
 
-mkdir obsidian-clipper
+Move-Item ~/.dotfiles/obsidian-clipper.zip -Destination ~/Downloads
+Set-Location ~/Downloads
+
 expand-archive "obsidian-clipper.zip" "obsidian-clipper" -Force
 Set-Location obsidian-clipper
 Get-ChildItem | Set-Location
 
+start-process edge://extensions
